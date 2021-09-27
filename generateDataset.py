2027cee9ddfe6361a850cfa0d8b427b694f16837
Thomas <thomas.schallert94@gmail.com>
2021-09-27 15:52:01 +0200

    feat: First Version of dataset generator
    Generator pulls the java projects with the most stars on github. Then filter the repositories that have less than 5000 commits. Get the five contributors with the most contributions for every projects. Then get all commits of these contributors for the project.

---
+++ generateDataset.py
@@ -0,0 +1,158 @@
+import requests
+from pprint import pprint
+import urllib
+import re
+
+# search commits with certain text
+token = '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'
+owner = "MartinHeinz"
+repo = "python-project-blueprint"
+query_url = f"https://api.github.com/search/commits"
+params = {
+    'q': 'fix',
+    'per_page': 1,
+}
+headers = {'Authorization': f'token {token}',
+           "Accept": "application/vnd.github.cloak-preview+json"}
+#r = requests.get(query_url, headers=headers, params=params)
+
+def sendRequest(url,params = None,header = None):
+    if(header == None):
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': f'token {token}',
+        }
+    if(params == None):
+        params = {
+            'per_page': 1,
+            'page': 1,
+        }
+    resp = requests.request('GET', url, params=params, headers=headers)
+    if (resp.status_code != 200):
+        raise Exception(f'invalid github response: {resp.content}')
+
+    return resp.json()
+
+def getBestJavaRepositories():
+    params = {
+        'q': 'stars:>10000 language:Java',
+        'per_page': 6,
+        'page': 1,
+    }
+    r = sendRequest("https://api.github.com/search/repositories",params = params)
+
+    return r['items']
+
+
+def commit_count(url, sha):
+    """
+    Return the number of commits to a project
+    """
+    params = {
+        'sha': sha,
+        'per_page': 1,
+    }
+    resp = sendRequest(url,params)
+    # check the resp count, just in case there are 0 commits
+    commit_count = len(resp)
+    last_page = resp.links.get('last')
+    #pprint(resp.json())
+    # if there are no more pages, the count must be 0 or 1
+    if last_page:
+        # extract the query string from the last page url
+        qs = urllib.parse.urlparse(last_page['url']).query
+        # extract the page number from the query string
+        commit_count = int(dict(urllib.parse.parse_qsl(qs))['page'])
+    return commit_count
+
+
+def getCommitMessageInRepoForUser(repoUrl, branch, userId):
+    params = {
+        'sha': branch,
+        'per_page': 1,
+        'page': 1,
+    }
+
+    resp = sendRequest(repoUrl,params)
+
+    pprint(resp)
+    messages = []
+    for commit in resp:
+        commitAuthorId = commit['author']['id']
+        print(commitAuthorId)
+        if(userId == commitAuthorId):
+            messages.append(commit['commit']['message'])
+    return messages
+
+def getIdsInMessage(msg):
+    # automatic linking of commits
+    # https://docs.github.com/en/github/writing-on-github/working-with-advanced-formatting/autolinked-references-and-urls#issues-and-pull-requests
+    r = re.compile(r'#([0-9]+)|\bgh-([0-9]+)\b', flags=re.I)
+    ids = []
+    id = None
+    for match in r.finditer(msg):
+        if(match.group(1)):
+            id = match.group(1)
+        if(match.group(2)):
+           id = match.group(2)
+        ids.append(id)
+    return ids
+
+def filterMessages(messages):
+    #check if list is empty
+    if messages:
+        for msg in messages:
+            continue
+
+def testGetCommitMessageInRepoForUser(token):
+    messages = getCommitMessageInRepoForUser("https://api.github.com/repos/elastic/elasticsearch/commits", "master", 40268737)
+    filterMessages(messages)
+    for msg in messages:
+        print("----------------")
+        print(msg)
+        ids = getIdsInMessage(msg)
+        print(ids)
+        for id in ids:
+            issue = sendRequest("https://api.github.com/repos/elastic/elasticsearch/issues/"+id)
+            print(issue['body'])
+            idsInIssue = getIdsInMessage(issue['body'])
+            for idInIssue in idsInIssue:
+                issue = sendRequest("https://api.github.com/repos/elastic/elasticsearch/issues/"+idInIssue)
+                print(issue['body'])
+        print("----------------")
+
+
+#repos = getBestJavaRepositories()
+repos = []
+ids = getIdsInMessage("#18 sdsdsdsd #10 asd gh-4 GH-20")
+testGetCommitMessageInRepoForUser(token)
+print(ids)
+sum = 0
+for repo in repos:
+    if(commit_count(repo['commits_url'][:-6], repo['default_branch'], token) > 5000):
+        print("--> " + repo['full_name'])
+        contribtors = sendRequest(repo['contributors_url'])
+        for contributor in contribtors:
+            contributorId = contributor['id']
+            getCommitMessageInRepoForUser(
+                repo['commits_url'][:-6], repo['default_branch'], contributorId)
+        sum = sum + 1
+    else:
+        print(repo['full_name'])
+
+
+# alle commits durchgehen
+# nur die rausfiltern die zu den 5 häufigsten commitern gehören
+# alle messages von denen ausgeben um zu sehen ob da nur müll dabei ist
+
+# conventional commits beachten
+# ---links zu issues und pr's resolven---
+# ---bot messages herausfiltern wie merge, auto commit, leere commits---
+
+
+# Zusamenfassung wie ein Paper zur Generierung eines Datasets die Daten abgespeichert hat
+# https://github.com/DeepSoftwareAnalytics/CommitMsgEmpirical/blob/main/dataset/ReadMe.md#raw-data
+
+
+print(str(sum)+"/"+str(len(repos)))
