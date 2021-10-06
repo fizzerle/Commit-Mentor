797249c450f4a1fc04dd7c6a46dba02490d5bd21
Thomas <thomas.schallert94@gmail.com>
2021-10-06 07:38:08 +0200

    feat(frontend): added angular material and installed first component

--- frontend/semi-automatic-commit-message-generation-app/src/app/app.module.ts
+++ frontend/semi-automatic-commit-message-generation-app/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GitDiffComponent } from './git-diff/git-diff.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitDiffComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/semi-automatic-commit-message-generation-app/src/app/git-diff/git-diff.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-git-diff',
+  templateUrl: './git-diff.component.html',
+  styleUrls: ['./git-diff.component.scss']
+})
+export class GitDiffComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
